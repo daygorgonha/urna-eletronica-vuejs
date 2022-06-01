490904b30a00aeeb1423a7153f2dc38fac5ee5a0
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-05-31 22:32:58 -0300

    feat:  added urn screen and its stylizations

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,18 @@
     
     <div class="urna">
 
-      <MeuTeclado />
+      <MinhaTela 
+       :tela="tela"
+       :numeroVoto="numeroVoto"
+       :quantidadeNumeros="quantidadeNumeros"
+       :candidato="candidato"
+      />
+      <MeuTeclado 
+        :adicionarNumero="adicionarNumero"
+        :corrigir="corrigir"
+        :confirmar="confirmar"
+        :votaremBranco="votaremBranco"
+      />
 
     </div>
   </div>
@@ -14,11 +25,139 @@
 import '@/css/global.css';
 
 import MeuTeclado from '@/components/MeuTeclado';
+import MinhaTela from '@/components/MinhaTela';
+
+import confirmAudio from '@/assets/audios/confirm.wav';
+import keyAudio from '@/assets/audios/key.wav';
 
 export default {
   name: 'App',
   components: {
-    MeuTeclado
+    MeuTeclado,
+    MinhaTela
+  },
+  methods:{
+    adicionarNumero(numero){
+      //EXECUTA SOM DA TECLA
+      this.executarSom(keyAudio);
+
+      // VERIFICA LIMITE DE NUMEROS VOTADOS
+      if(this.numeroVoto.length == this.quantidadeNumeros){
+        return false;
+      }
+
+      //ADICIONA O NUMERO SELECIONADO
+      this.numeroVoto += ''+numero;
+
+      //VERIFICA CANDIDATO VOTADO
+      this.verificarCandidato();
+    },
+      verificarCandidato(){
+        //VOTO INCOMPLETO
+        if(this.numeroVoto.length < this.quantidadeNumeros){
+          return false;
+        }
+
+        //VERIFICA CANDIDATO EXISTENTE
+        if(this.candidatos[this.tela][this.numeroVoto]){
+          this.candidato = this.candidatos[this.tela][this.numeroVoto];
+          return true;
+        }
+
+        //VOTO NULO
+        this.candidato = {
+          nome: 'Voto nulo',
+          partido: 'Voto nulo',
+          imagem: ''
+        }
+      },
+      corrigir(){
+        //EXECUTA SOM DA TECLA
+        this.executarSom(keyAudio);
+        this.limpar();
+      },
+      limpar(){
+        this.candidato = {}
+        this.numeroVoto = ''
+      },
+      confirmar(){
+        //VOTO INCOMPLETO
+        if(this.numeroVoto.length < this.quantidadeNumeros){
+          return false;
+        }
+
+        return this.avancarTela();
+      },
+      avancarTela(){
+        //EXECUTA SOM DA TECLA
+        this.executarSom(confirmAudio);
+
+        //VEREADOR
+        if(this.tela == 'prefeito'){
+          this.tela = 'vereador';
+          this.quantidadeNumeros = 5;
+          return this.limpar();
+        }
+        //FINALIZACAO
+        this.tela = 'fim';
+
+        //INSTANCIA ATUAL
+        var instancia = this;
+
+        //VOLTAR AO INICIO
+        setTimeout(function(){
+          instancia.tela = 'prefeito';
+          instancia.quantidadeNumeros = 2;
+          return instancia.limpar();
+        },3000);
+      },
+      votaremBranco(){
+        //TELA DE FINALIZACAO
+        if(this.tela == 'fim') return false;
+
+        this.limpar();
+        this.avancarTela();
+      },
+      executarSom(arquivoSom){
+        if(arquivoSom){
+          let audio = new Audio(arquivoSom);
+          audio.play();
+        }
+      }
+   },
+   data(){
+    return {
+      tela: 'prefeito',
+      numeroVoto: '',
+      quantidadeNumeros: 2,
+      candidato:{},
+      candidatos: {
+          "prefeito":{
+            "01":{
+              "nome": "Ash",
+              "partido": "Pokemon",
+              "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/ash.png"
+            },
+            "08":{
+              "nome": "Vegeta",
+              "partido": "Dragon Ball",
+              "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/vegeta.png"
+            }
+          },
+          "vereador":{
+            "01234":{
+              "nome": "Pikachu",
+              "partido": "Pokemon",
+              "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/pikachu.png"
+            },
+            "08001":{
+              "nome": "Goku",
+              "partido": "Dragon Ball",
+              "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/goku.png"
+            }
+          }
+       }
+    }
   }
 }
 </script>
@@ -40,5 +179,6 @@ export default {
   padding: 30px;
   border-radius: 5px;
   display: flex;
+  justify-content: space-between;
 }
 </style>
